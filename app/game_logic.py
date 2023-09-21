a28193afc2ef05b3c044798764da33fe6d9d5f57
Oscar Lysak <oscar.lysak@gmail.com>
2023-09-21 11:39:15 +0100

    Added comments for readability

--- app/game_logic.py
+++ app/game_logic.py
@@ -1,42 +1,52 @@
 from random import randint
 from .models import User, Adventure, LootBox, PrizeType, Prize
 from . import db
-
 from datetime import datetime, timedelta
 
+# The AdventureManager manages the logic for user adventures.
 class AdventureManager:
-
+    # The constructor initializes the manager with a user.
     def __init__(self, user):
         self.user = user
-   
+
+    # Static method to check if a user is eligible for an adventure.
     @staticmethod
     def is_eligible(user):
+        # Fetch the last adventure of the user.
         last_adventure = Adventure.query.filter_by(user_id=user.id).order_by(Adventure.timestamp.desc()).first()
+        # If there's a last adventure, check if it's been more than a day since the last adventure.
         if last_adventure:
             now = datetime.utcnow()
             time_since_last_adventure = now - last_adventure.timestamp
             return time_since_last_adventure > timedelta(days=1)
+        # If no previous adventure, the user is eligible.
         return True
 
+    # Create a new adventure for the user.
     def create(self):
         rng_score = self._generate_random_number()
         material = self._determine_material(rng_score)
         self._update_user_threshold(material)
         
+        # Create a new adventure record and add it to the session.
         new_adventure = Adventure(rng_score=rng_score, material=material, user_id=self.user.id, status="In Progress")
         db.session.add(new_adventure)
-        
+
+        # Update the status based on the material.
         if material == "None":
             new_adventure.status = "No Material"
         else:
             new_adventure.status = "Unused Material"
 
+        # Commit the changes to the database.
         db.session.commit()
         return new_adventure
 
+    # Generate a random number between 1 and the user's current threshold.
     def _generate_random_number(self):
         return randint(1, self.user.current_threshold)
-    
+
+    # Determine the material based on the generated random number.
     def _determine_material(self, rng_score):
         if rng_score < 5:
             return "Legendary"
@@ -50,7 +60,8 @@ class AdventureManager:
             return "Common"
         else:
             return "None"
-        
+
+    # Update the user's threshold based on the material.
     def _update_user_threshold(self, material):
         if material in ["Uncommon", "Common", "None"]:
             self.user.current_threshold -= 10
@@ -60,43 +71,45 @@ class AdventureManager:
                 self.user.reset_threshold -= 5
             self.user.current_threshold = max(self.user.current_threshold, 400)
 
-
+# The LootBoxManager manages the logic for creating lootboxes.
 class LootBoxManager:
-    
+    # The constructor initializes the manager with a user and material IDs.
     def __init__(self, user, material_ids):
         self.user = user
         self.material_ids = material_ids
 
+    # Create a lootbox.
     def create(self):
-        # Validate material IDs and update their statuses
+        # Fetch the adventures corresponding to the material IDs.
         adventures = Adventure.query.filter(Adventure.id.in_(self.material_ids)).all()
-        sum_rng_scores = 0  # Initialize sum of rng_scores
-
+        sum_rng_scores = 0
+        # Validate the adventures and sum up their rng_scores.
         for adventure in adventures:
             if adventure.status != "Unused Material":
                 return "Error: Cannot use already used or ineligible material"
-            sum_rng_scores += adventure.rng_score  # Summing up the rng_scores
+            sum_rng_scores += adventure.rng_score
 
-        # Determine the rarity based on the sum of rng_scores
+        # Determine the lootbox rarity based on the summed rng_scores.
         rarity = self._determine_lootbox_rarity(sum_rng_scores)
 
+        # Update the adventure statuses to "Used Material".
         for adventure in adventures:
             adventure.status = "Used Material"
 
-        # Create new lootbox
+        # Create a new lootbox record and add it to the session.
         new_lootbox = LootBox(rarity=rarity, user_id=self.user.id)
         db.session.add(new_lootbox)
 
-        # Create a new prize based on the lootbox's rarity
+        # Create a new prize for the user based on the lootbox's rarity.
         prize_manager = PrizeManager(self.user, new_lootbox)
         new_prize = prize_manager.create()
 
-        # Commit all changes to the database
+        # Commit the changes to the database.
         db.session.commit()
 
-        return new_lootbox, new_prize  # Return both the new lootbox and the new prize
+        return new_lootbox, new_prize
 
-    # Additional method to determine lootbox rarity
+    # Determine lootbox rarity based on the summed rng_scores.
     def _determine_lootbox_rarity(self, sum_rng_scores):
         scaled_sum = sum_rng_scores / 5
         if scaled_sum < 5:
@@ -109,25 +122,27 @@ class LootBoxManager:
             return "Uncommon"
         else:
             return "Common"
-    
+
+# The PrizeManager manages the logic for awarding prizes.
 class PrizeManager:
-    
+    # The constructor initializes the manager with a user and a lootbox.
     def __init__(self, user, lootbox):
         self.user = user
         self.lootbox = lootbox
 
+    # Create a prize for the user.
     def create(self):
-        # Fetch a prize based on the lootbox's rarity
+        # Fetch a prize type based on the lootbox's rarity that hasn't been fully claimed yet.
         prize_type = PrizeType.query.filter_by(rarity=self.lootbox.rarity).filter(PrizeType.number_claimed < PrizeType.quanity).first()
 
         if not prize_type:
             return "Error: No available prize for this rarity"
 
-        # Create a new Prize record
+        # Create a new prize record for the user and add it to the session.
         new_prize = Prize(user_id=self.user.id, prize_type_id=prize_type.id)
         db.session.add(new_prize)
 
-        # Update the number_claimed for the prize type
+        # Increment the number_claimed counter for the prize type.
         prize_type.number_claimed += 1
 
-        return new_prize  # Return the new prize
+        return new_prize
--- app/routes.py
+++ app/routes.py
@@ -1,69 +1,97 @@
 from flask import Blueprint, jsonify, request
 from collections import Counter
-from .models import User, Adventure, LootBox  
+from .models import User, Adventure
 from .game_logic import AdventureManager, LootBoxManager  
 
+
+# 'main' is the Blueprint name which will be imported and registered in the Flask app.
 main = Blueprint('main', __name__)
 
+# Define an endpoint for the adventure creation functionality.
 @main.route('/adventure', methods=['POST'])
 def adventure_endpoint():
+    # Extract the request data as JSON.
     user_data = request.get_json()
-    user_id = user_data['user_id']
     
+    # Extract the user ID from the incoming data.
+    user_id = user_data['user_id']
+
+    # Try to get the user from the database with the provided ID.
     user = User.query.get(user_id)
+    
+    # If the user is not found in the database, return an error message.
     if not user:
         return jsonify({'message': 'User not found'}), 404
 
+    # Check if the user is eligible to go on an adventure.
+    # The logic for eligibility is encapsulated within the AdventureManager class.
     if not AdventureManager.is_eligible(user):
         return jsonify({'message': 'You can only go on one adventure per day'}), 403
-    
+
+    # Create a new adventure for the user using the AdventureManager class.
     adventure_manager = AdventureManager(user)
     new_adventure = adventure_manager.create()
-    
+
+    # Return a success message along with the material the user got from the adventure.
     return jsonify({'message': 'Adventure complete', 'material': new_adventure.material}), 201
 
+# Define an endpoint to retrieve a summary of the user's materials.
 @main.route('/materials_summary', methods=['GET'])
 def get_materials_summary():
+    # Extract the user ID from the request arguments.
     user_data = request.args
     user_id = user_data.get('user_id', None)
-    
+
+    # Check if a user ID was provided.
     if not user_id:
         return jsonify({'message': 'User ID is required'}), 400
 
+    # Try to get the user from the database with the provided ID.
     user = User.query.get(user_id)
+
+    # If the user is not found in the database, return an error message.
     if not user:
         return jsonify({'message': 'User not found'}), 404
 
+    # Get all adventures associated with the user from the database.
     all_adventures = Adventure.query.filter_by(user_id=user.id).all()
 
-    # Separate the adventures into used and unused based on their status
+    # Separate the adventures based on whether the materials were used or not.
     used_adventures = [adv for adv in all_adventures if adv.status == "Used Material"]
     unused_adventures = [adv for adv in all_adventures if adv.status == "Unused Material"]
 
-    # Count the occurrences of each type of material
+    # Count the occurrences of each type of material.
     used_material_counts = Counter([adv.material for adv in used_adventures])
     unused_material_counts = Counter([adv.material for adv in unused_adventures])
 
+    # Return a summary of both used and unused materials.
     return jsonify({
         'used_materials_summary': used_material_counts,
         'unused_materials_summary': unused_material_counts
     }), 200
 
-
+# Define an endpoint to retrieve a user's adventure history.
 @main.route('/adventure_history', methods=['GET'])
 def get_adventure_history():
+    # Extract the user ID from the request arguments.
     user_data = request.args
     user_id = user_data.get('user_id', None)
-    
+
+    # Check if a user ID was provided.
     if not user_id:
         return jsonify({'message': 'User ID is required'}), 400
 
+    # Try to get the user from the database with the provided ID.
     user = User.query.get(user_id)
+
+    # If the user is not found in the database, return an error message.
     if not user:
         return jsonify({'message': 'User not found'}), 404
 
+    # Get all adventures associated with the user from the database, ordered by timestamp.
     adventures = Adventure.query.filter_by(user_id=user.id).order_by(Adventure.timestamp.desc()).all()
-    
+
+    # Prepare a list of adventure history details to return.
     adventure_history = [
         {
             'id': adventure.id,
@@ -72,29 +100,38 @@ def get_adventure_history():
         }
         for adventure in adventures
     ]
-    
+
+    # Return the user's adventure history.
     return jsonify({'adventure_history': adventure_history}), 200
 
-
-
+# Define an endpoint to create a lootbox.
 @main.route('/forge_lootbox', methods=['POST'])
 def create_lootbox():
+    # Extract the request data as JSON.
     data = request.get_json()
-    user_id = data['user_id']
-    material_ids = data['material_ids']  # An array of 5 material (adventure) IDs
 
+    # Extract the user ID and material IDs from the incoming data.
+    user_id = data['user_id']
+    material_ids = data['material_ids']
+
+    # Try to get the user from the database with the provided ID.
     user = User.query.get(user_id)
+
+    # If the user is not found in the database, return an error message.
     if not user:
         return jsonify({'message': 'User not found'}), 404
 
+    # Check if the correct number of materials (5) were provided.
     if len(material_ids) != 5:
         return jsonify({'message': 'Exactly 5 materials are required to forge a LootBox'}), 400
 
+    # Create a new lootbox using the LootBoxManager class.
     lootbox_manager = LootBoxManager(user, material_ids)
     new_lootbox = lootbox_manager.create()
 
+    # If there was an error in creating the lootbox (e.g., using already used materials), return an error message.
     if new_lootbox == "Error: Cannot use already used or ineligible material":
         return jsonify({'message': new_lootbox}), 400
-    
-    return jsonify({'message': 'New LootBox created', 'rarity': new_lootbox.rarity}), 201
 
+    # Return a success message along with the rarity of the created lootbox.
+    return jsonify({'message': 'New LootBox created', 'rarity': new_lootbox.rarity}), 201
